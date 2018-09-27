75b48951cf0046acc08ad839deab6c6869ec7025
leo <18505650602@163.com>
2018-09-27 16:11:34 +0800

    test

--- js/common/main.js
+++ js/common/main.js
@@ -100,40 +100,40 @@
 
 }());
 $(document).ready(function() {
-        $("nav .nav").append("<li><a href ='index.html'>Index</a><i></i></li><li><a href ='page2.html'>Photos</a><i></i></li><li><a href = 'page3.html'>Basketball</a><i></i></li><li><div id='tp-weather-widget' style='width:300px;float:left'></div></li>");
-        $('.nav li a').each(function() {
-            if ($($(this))[0].href == String(window.location))
-                $(this).parent().addClass('cur');
-        });
+    $("nav .nav").append("<li><a href ='index.html'>Index</a><i></i></li><li><a href ='page2.html'>Photos</a><i></i></li><li><a href = 'page3.html'>Basketball</a><i></i></li><li><div id='tp-weather-widget' style='float:left'></div></li>");
+    $('.nav li a').each(function() {
+        if ($($(this))[0].href == String(window.location))
+            $(this).parent().addClass('cur');
+    });
 
-    })
-    //     (function(T, h, i, n, k, P, a, g, e) {
-    //         g = function() {
-    //             P = h.createElement(i);
-    //             a = h.getElementsByTagName(i)[0];
-    //             P.src = k;
-    //             P.charset = "utf-8";
-    //             P.async = 1;
-    //             a.parentNode.insertBefore(P, a)
-    //         };
-    //         T["ThinkPageWeatherWidgetObject"] = n;
-    //         T[n] || (T[n] = function() {
-    //             (T[n].q = T[n].q || []).push(arguments)
-    //         });
-    //         T[n].l = +new Date();
-    //         if (T.attachEvent) { T.attachEvent("onload", g) } else { T.addEventListener("load", g, false) }
-    //     }(window, document, "script", "tpwidget", "//widget.seniverse.com/widget/chameleon.js"));
-    // tpwidget("init", {
-    //     "flavor": "slim",
-    //     "location": "WTW3SJ5ZBJUY",
-    //     "geolocation": "enabled",
-    //     "language": "zh-chs",
-    //     "unit": "c",
-    //     "theme": "chameleon",
-    //     "container": "tp-weather-widget",
-    //     "bubble": "enabled",
-    //     "alarmType": "badge",
-    //     "uid": "UE15465C00",
-    //     "hash": "436b4f07f26fb2c11acef727b1eb1e05"
-    // });
-    // tpwidget("show");
+});
+(function(T, h, i, n, k, P, a, g, e) {
+    g = function() {
+        P = h.createElement(i);
+        a = h.getElementsByTagName(i)[0];
+        P.src = k;
+        P.charset = "utf-8";
+        P.async = 1;
+        a.parentNode.insertBefore(P, a)
+    };
+    T["ThinkPageWeatherWidgetObject"] = n;
+    T[n] || (T[n] = function() {
+        (T[n].q = T[n].q || []).push(arguments)
+    });
+    T[n].l = +new Date();
+    if (T.attachEvent) { T.attachEvent("onload", g) } else { T.addEventListener("load", g, false) }
+}(window, document, "script", "tpwidget", "//widget.seniverse.com/widget/chameleon.js"));
+tpwidget("init", {
+    "flavor": "slim",
+    "location": "WTW3SJ5ZBJUY",
+    "geolocation": "enabled",
+    "language": "zh-chs",
+    "unit": "c",
+    "theme": "chameleon",
+    "container": "tp-weather-widget",
+    "bubble": "enabled",
+    "alarmType": "badge",
+    "uid": "UE15465C00",
+    "hash": "436b4f07f26fb2c11acef727b1eb1e05"
+});
+tpwidget("show");
